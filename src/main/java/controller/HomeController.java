6d9e918a6511f5233b7d95fc291146e4255c3785
GD <GD@DESKTOP-66MHREQ>
2025-11-03 10:31:36 +0900

    커밋 연습2 - home.jsp

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -10,32 +10,13 @@ import java.io.IOException;
 /**
  * Servlet implementation class HomeController
  */
-@WebServlet("/HomeController")
+@WebServlet("/homeController")
 public class HomeController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HomeController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/WEB-INF/view/home.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
 }
